972243f61a38444063db873e00eab8651498d1c3
ricardosantanadev4 <ricardosantanadev4@gmail.com.br>
2022-04-11 20:03:04 -0300

    Front End Java Aula1 Módulo4

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function age() {
     // console.log(y1)
 
     // Date e uma classe padrao do js
-    // inicialisando um objeto dessa classe e possivel pegar a data atual
+    // inicializando um objeto dessa classe e possivel pegar a data atual
     var date = new Date();
     // Pega o dia atual
     var d2 = date.getDate();
@@ -35,19 +35,21 @@ function age() {
     // vai ser usado na base de calculos
     var month = [31,28,31,30,31,30,31,31,30,31,30,31]
     
-    // debugger poto de parada para o debugger 
+    // debugger ponto de parada para o debugger 
     debugger;
 
     //  se a data de nascimento for maior que a atual entra nessa condicao
     if(d1 > d2){         
         // soma o total de dias do mes anterior + a quantidade de dias de hoje
-        //  usa o resul mais na fre para achar a quantidade de dias 
+        //  usa o resultado mais na frente para achar a quantidade de dias 
         d2 = d2 + month[m2 - 1];
-        //  tem que retirar menos novamente porque o computador conta a partir do 0
-        //  ex se for 3 meses para o computador e 2 meses que vai de 0 1 2
+        //  tem que retirar -1 novamente porque o computador conta a partir do 0
+        //  para o usuario 3 meses e igual 3 porque ele unicia a contagem a partir do1 para o computador 3 meses e igual a 2
+        //  porque o computador vai contar 0 1 2
         m2 = m2 - 1;
     }
 
+    //  se o mes de nascimento for maior que o mes atual entra na condicao
     if(m1 > m2){
         m2 = m2 + 12;
         y2 = y2 - 1;
@@ -55,7 +57,7 @@ function age() {
 
     debugger;
     // encontra a quantidade de dias
-    // formula: total de dias do mes anterior + o dia atual - dia de nascimento
+    // (total de dias do mes anterior + o dia atual) - o dia de nascimento
     var d = d2 - d1;
     // encontra a quantidade de meses
     //  para o usuario vai aparecer um mes a mesnos porque o computador conta os meses apartir do 0
@@ -67,11 +69,13 @@ function age() {
     console.log(m)
     console.log(y)
 
-    // innerHTML insere no HTML nesse caso esta inserido a string no HTML
-    // pega o valor insere na div do html que contem o id = a age
+    // innerHTML insere no HTML, nesse caso esta inserido a string no HTML
+    // depoies o pega o valor insere na div do html que contem o id = a age
     document.getElementById('age').innerHTML = "sua idade e: " + y + " anos " + m +"meses e " + d + " dias";
 }
 
+//  essa funcao e acionada pelo onchange contido no input de data do HTML
+//  exibe o valor digitado no input em tempo real
 function ageChange(){
     var d1 = document.getElementById('date').value;
     console.log(d1)
